d1aead189a15474652ccb22dd0717a909a7ce73f
zlatandp <zlatandnepr@gmail.com>
2025-03-31 15:15:09 +0300

    added mobile version for frame and nigth-light sections, refactoring animation code

--- js/animations.js
+++ js/animations.js
@@ -15,31 +15,34 @@ function isMobile() {
   return window.innerWidth < 760;
 }
 
-if (moveHereFrame && tvWrap) {
-  tvWrap.addEventListener('mouseenter', () =>
-    toggleHiddenAnimation(moveHereFrame, tv1, tv2)
-  );
-  tvWrap.addEventListener('mouseleave', () =>
-    toggleHiddenAnimation(moveHereFrame, tv1, tv2)
-  );
-  moveHereFrame.addEventListener('mouseenter', () =>
-    toggleHiddenAnimation(moveHereFrame, tv1, tv2)
-  );
-  moveHereFrame.addEventListener('mouseleave', () =>
-    toggleHiddenAnimation(moveHereFrame, tv1, tv2)
-  );
+function handleFrameHover() {
+  toggleHiddenAnimation(moveHereFrame, tv1, tv2);
+}
 
-  tvWrap.addEventListener('click', () => {
-    if (isMobile()) {
-      toggleHiddenAnimation(moveHereFrame, tv1, tv2);
-    }
-  });
+function handleFrameClick() {
+  toggleHiddenAnimation(tv1, tv2);
+}
 
-  moveHereFrame.addEventListener('click', () => {
-    if (isMobile()) {
-      toggleHiddenAnimation(moveHereFrame, tv1, tv2);
-    }
-  });
+function addFrameListeners() {
+  if (!isMobile()) {
+    tvWrap.addEventListener('mouseenter', handleFrameHover);
+    tvWrap.addEventListener('mouseleave', handleFrameHover);
+    moveHereFrame.addEventListener('mouseenter', handleFrameHover);
+    moveHereFrame.addEventListener('mouseleave', handleFrameHover);
+  } else {
+    tvWrap.addEventListener('click', handleFrameClick);
+    moveHereFrame.addEventListener('click', handleFrameClick);
+  }
+}
+
+function removeFrameListeners() {
+  tvWrap.removeEventListener('mouseenter', handleFrameHover);
+  tvWrap.removeEventListener('mouseleave', handleFrameHover);
+  moveHereFrame.removeEventListener('mouseenter', handleFrameHover);
+  moveHereFrame.removeEventListener('mouseleave', handleFrameHover);
+
+  tvWrap.removeEventListener('click', handleFrameClick);
+  moveHereFrame.removeEventListener('click', handleFrameClick);
 }
 
 // Night animation
@@ -48,28 +51,48 @@ const moveHereNight = document.querySelector('.move-here-night');
 const nightLightImg = document.querySelector('.night-light-img-second');
 const nightLightTrigger = document.querySelector('.night-light-trigger');
 
-if (moveHereNight && nightLightImg) {
-  moveHereNight.addEventListener('mouseenter', () =>
-    toggleHiddenAnimation(moveHereNight, nightLightImg)
-  );
-  moveHereNight.addEventListener('mouseleave', () =>
-    toggleHiddenAnimation(moveHereNight, nightLightImg)
-  );
-  nightLightTrigger.addEventListener('mouseenter', () =>
-    toggleHiddenAnimation(moveHereNight, nightLightImg)
-  );
-  nightLightTrigger.addEventListener('mouseleave', () =>
-    toggleHiddenAnimation(moveHereNight, nightLightImg)
-  );
-
-  moveHereNight.addEventListener('click', () => {
-    if (isMobile()) {
-      toggleHiddenAnimation(moveHereNight, nightLightImg);
-    }
-  });
-  nightLightTrigger.addEventListener('click', () => {
-    if (isMobile()) {
-      toggleHiddenAnimation(moveHereNight, nightLightImg);
-    }
-  });
+function handleNightHover() {
+  toggleHiddenAnimation(moveHereNight, nightLightImg);
 }
+
+function handleNightClick() {
+  toggleHiddenAnimation(nightLightImg);
+}
+
+function addNightListeners() {
+  if (!isMobile()) {
+    nightLightTrigger.addEventListener('mouseenter', handleNightHover);
+    nightLightTrigger.addEventListener('mouseleave', handleNightHover);
+    moveHereNight.addEventListener('mouseenter', handleNightHover);
+    moveHereNight.addEventListener('mouseleave', handleNightHover);
+  } else {
+    nightLightTrigger.addEventListener('click', handleNightClick);
+    moveHereNight.addEventListener('click', handleNightClick);
+  }
+}
+
+function removeNightListeners() {
+  nightLightTrigger.removeEventListener('mouseenter', handleNightHover);
+  nightLightTrigger.removeEventListener('mouseleave', handleNightHover);
+  moveHereNight.removeEventListener('mouseenter', handleNightHover);
+  moveHereNight.removeEventListener('mouseleave', handleNightHover);
+
+  nightLightTrigger.removeEventListener('click', handleNightClick);
+  moveHereNight.removeEventListener('click', handleNightClick);
+}
+
+function updateListeners() {
+  removeFrameListeners();
+  addFrameListeners();
+  removeNightListeners();
+  addNightListeners();
+}
+
+let resizeTimeout;
+window.addEventListener('resize', () => {
+  clearTimeout(resizeTimeout);
+
+  resizeTimeout = setTimeout(updateListeners, 200);
+});
+
+updateListeners();
